4ae7d1ec9d5fc43d4aa4605f3402583d15d9c8f5
Piyush Kumar Singh <21bcs1638@cuchd.in>
2024-02-18 00:53:00 +0530

    posts implemented

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,48 @@
+import Posts from "@/components/shared/Posts";
+import Image from "next/image";
+import { CiSearch } from "react-icons/ci";
+
+
+const page = () => {
+  return (
+    <section className="min-h-screen bg-[#041220] py-32">
+      <div className="w-full bg-[#15263A] flex items-center justify-center py-8">
+        <h1 className="text-[#DCCDB0] text-5xl font-semibold">Boost Your Circle</h1>
+      </div>
+      <div className="flex items-center flex-row ">
+        <Image
+          src="\assets\images\post.svg"
+          alt="post"
+          height={900}
+          width={900}
+        />
+        <div className="flex flex-col relative justify-center h-full w-full gap-3">
+            <h3 className="text-[#DCCDB0] text-4xl font-bold">Explore Posts</h3>
+            <div className="border border-[#DCCDB0] flex items-center rounded-full px-3 py-3 w-4/5 shadow-[#DCCDB0] shadow-sm">
+                <input type="text" placeholder="Search..." className="bg-transparent outline-none w-full text-slate-50 " />
+                <CiSearch className="text-[#DCCDB0] mx-3 text-3xl"/>
+            </div>
+            <div className="flex items-center flex-row gap-4 mt-4">
+                <button className="text-[#D9D9D9] bg-gray-700 px-3 py-2 rounded-full font-medium">Prompts</button>
+                <button className="text-[#D9D9D9] bg-gray-700 px-3 py-2 rounded-full font-medium">Posts</button>
+                <button className="text-[#D9D9D9] bg-gray-700 px-3 py-2 rounded-full font-medium">Repos</button>
+            </div>
+        </div>
+      </div>
+
+      <div className="flex items-center flex-col justify-center mt-10 gap-3">
+        <Posts/>
+        <Posts/>
+        <Posts/>
+        <Posts/>
+        <Posts/>
+        <Posts/>
+        <Posts/>
+        <Posts/>
+
+      </div>
+    </section>
+  );
+};
+
+export default page;
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,42 +1,55 @@
-"use client"
-import axios from 'axios'
-import { useRouter } from 'next/navigation';
-import React from 'react'
+"use client";
+import ProfileCard from "@/components/shared/Posts";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React from "react";
+import Image from "next/image";
 
 const Profile = () => {
   const router = useRouter();
-  const onLogout = async()=> {
-      try {
-        await axios('/api/users/logout');
-        console.log("Logout successful!");
-        router.push("/login")
-      } catch (error:any) {
-        console.log("Logout failed!");
-        console.log(error.message)
-      }
-
-  }
-
+  const onLogout = async () => {
+    try {
+      await axios("/api/users/logout");
+      console.log("Logout successful!");
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Logout failed!");
+      console.log(error.message);
+    }
+  };
 
   const getUserDetails = async () => {
     try {
       const res = await axios.get("/api/users/me");
-      console.log("user details",res.data);
+      console.log("user details", res.data);
       router.push(`/profile/${res.data.data._id}`);
-    } catch (error:any) {
-      console.log("error",error.message);
+    } catch (error: any) {
+      console.log("error", error.message);
     }
-  }
+  };
   return (
-    <div className='flex items-center flex-col justify-center min-h-screen '>
-      <h1>Profile</h1>
-      <button
-      onClick={onLogout}
-      className='bg-none hover:bg-slate-300 hover:text-black hover:border-none border my-6 border-slate-50 rounded-lg px-3 py-2'
-      >Logout</button>
-      <button onClick={getUserDetails} className='text-black bg-slate-200 px-2 py-2 rounded-lg'>Get my details</button>
-    </div>
-  )
-}
+    <section className="flex items-center justify-center min-h-screen px-24 bg-[#041220] py-32">
+      <div className="flex items-center flex-row">
+        <div className="left-div ">
+          <div className="flex items-center ">
+            <button className="px-3 py-2 r">My Posts</button>
+            <button>My Repos</button>
+            <button>My Prompts</button>
+            <button>Saved Posts</button>
+          </div>
+          <div>
+            <ProfileCard/>
+          </div>
+        </div>
+        <div className="right-div flex-[1] flex flex-col">
+          <h1>Profile</h1>
+          <div>
+             <Image src="/assets/images/User.png" alt="User" height={60} width={60}/>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Profile
+export default Profile;
